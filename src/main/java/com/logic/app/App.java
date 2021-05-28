1c31e661ecade7750019e9f52de4c641e3c13eb5
Emilia Kwolek <305350@student.agh.edu.pl>
2021-05-28 09:46:20 +0200

    Change order of arguments

--- src/main/java/com/logic/app/App.java
+++ src/main/java/com/logic/app/App.java
@@ -6,18 +6,22 @@ public class App
 
     public static void main( String[] args ) throws IOException {
         int lengths = args.length;
-        if (lengths < 2){
+        if (lengths < 1){
             System.out.println("Wrong amount of arguments, expected LOGIC word, phrase to check " +
                     "and optionally filepath to special characters file in txt format");
             System.exit(1);
+        }
+        String logicString = "LOGIC";
+        if(lengths > 1){
+            logicString = args[1];
         }
         String fileName = null;
         if(lengths == 3){
             fileName = args[2];
         }
 
-        Solution solution = new Solution(new Configuration(args[0], fileName));
-        solution.solve(args[1]);
+        Solution solution = new Solution(new Configuration(logicString, fileName));
+        solution.solve(args[0]);
         solution.printSolution();
     }
 }
